bd84f0406f9bbd6a95d1b0a97f9d0cc7dc05c76b
CaraH03 <1304164@rgu.ac.uk>
2016-10-10 16:26:36 +0100

    Initional set up

--- DBconnect.php
+++ DBconnect.php
@@ -1,9 +1,8 @@
 <?php
-//connect to csdm webdev server and select database
 $db = new mysqli(
-    'ap-cdbr-azure-east-c.cloudapp.net'
-    'bba1eb6bf04dc6'
-    '672863d4'
+    'ap-cdbr-azure-east-c.cloudapp.net',
+    'bba1eb6bf04dc6',
+    '672863d4',
     'cara'
 );
 if($db->connect_errno){
