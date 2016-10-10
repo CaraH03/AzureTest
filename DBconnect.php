c1dceae9bd783390b2cddc50ce520b6672a60f29
CaraH03 <1304164@rgu.ac.uk>
2016-10-10 16:51:26 +0100

    Initional set up

--- DBconnect.php
+++ DBconnect.php
@@ -1,4 +1,6 @@
 <?php
+include("dbconnect.php");
+
 $db = new mysqli(
     'ap-cdbr-azure-east-c.cloudapp.net',
     'bba1eb6bf04dc6',
@@ -14,5 +16,5 @@ $sql_query="SELECT * FROM marvelmovies WHERE yearReleased LIKE '%1995%'";
 $result = $db->query($sql_query);
 
 while($row=$result->fetch_array()){
-
+echo "<p>{$row['title']}</p>";
 }
